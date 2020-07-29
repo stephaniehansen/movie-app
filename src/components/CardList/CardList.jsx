74a05f3dca38102916ab766ec7045482062b126f
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:25:07 +0100

    feat: display movies from sample data

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import Card from "../Card";
 
 export default class CardList extends Component {
+
+  getMovies = () => {
+    return this.props.movies.map(movie => <Card movie={movie} />)
+  }
 
   render() { 
     return (
       <section>
         <p>CardList</p>
+        {this.getMovies()}
       </section>
-
     );
   }
 }
