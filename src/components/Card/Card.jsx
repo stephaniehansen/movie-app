7e0ed75ea3fe43c74f2233dff292c476bce89148
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 17:31:47 +0100

    feat: add default poster when none available

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import styles from "./Card.module.scss";
+import defaultPoster from "../../assets/movie-poster-default.png"
 
 export default class Card extends Component {
+
   render() { 
-    const {Title, Year, Poster} = this.props.movie;
+    const {Title, Poster, Year } = this.props.movie;
     return (
         <div className={styles.card}>
-          <img src={Poster} alt={Title} />
+          <img src={Poster !== "N/A" ? Poster : defaultPoster} alt={Title} />
           <h3>{Title}</h3>
           <p>{Year}</p>
         </div>
