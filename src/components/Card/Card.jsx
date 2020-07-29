d6c95b54a2d8e7c3045119ef71f74704f030c98a
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 21:26:16 +0100

    feat: fetch more info

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,15 @@ import styles from "./Card.module.scss";
 import defaultPoster from "../../assets/movie-poster-default.png"
 
 export default class Card extends Component {
-
   render() { 
-    const {Title, Poster, Year } = this.props.movie;
+    const { Title, Poster, Year, imdbID } = this.props.movie;
+    const { fetchInfo } = this.props;
+
     return (
-        <div className={styles.card}>
+        <div className={styles.card} onClick={() => fetchInfo(imdbID)}>
           <img src={Poster !== "N/A" ? Poster : defaultPoster} alt={Title} />
           <h3>{Title}</h3>
-          <p>{Year}</p>
+          <p className={styles.releaseYear}>{Year}</p>
         </div>
     );
   }
