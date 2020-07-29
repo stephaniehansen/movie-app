0b2b8fd46c3077d5dd1835c831391db514b643ed
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:26:14 +0100

    feat: display movies from sample data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import NavBar from "./components/NavBar";
+import CardList from "./components/CardList";
+
+import movies from "./data/movies";
 
 export default class App extends Component {
+  state = {
+    movies: movies
+  }
   render() { 
     return ( 
       <>
         <NavBar />
+        <CardList movies={this.state.movies}/>
       </>
      );
   }
