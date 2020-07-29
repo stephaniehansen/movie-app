bdcc0243b48fd196f1d35206077274b5a5761b0d
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 13:00:44 +0100

    feat: add ability to search api data

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,11 @@ import SearchBar from "../SearchBar";
 
 export default class NavBar extends Component {
   render() {
-    const { updateSearch } = this.props;
+    const { updateSearch, handleChange } = this.props;
     return (
       <nav className={styles.navBar}>
         <p>Movies</p>
-        <SearchBar placeholder="Search movies..." updateSearch={updateSearch} />
+        <SearchBar placeholder="Search movies..." updateSearch={updateSearch} handleChange={handleChange} />
       </nav>
     );
   }
