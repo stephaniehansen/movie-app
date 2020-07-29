844d9174c3b099d8e16291d385bd0fcab9e4ef50
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 11:53:32 +0100

    feat: fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,26 @@ export default class App extends Component {
 
   updateSearch = searchTerm => this.setState({ searchTerm });
 
+  fetchMovies = (searchTerm) => {
+    fetch(`http://www.omdbapi.com/?s=harry&potter&apikey=f10a5202`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ movies: data.Search })})
+      .catch(error => console.log(error));
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
   render() { 
     const { movies, searchTerm } = this.state;
     return ( 
       <>
-        <NavBar updateSearch={this.updateSearch} />
+        <NavBar updateSearch={this.fetchMovies} />
         <CardList movies={movies} searchTerm={searchTerm} />
       </>
      );
   }
 }
+
