ea6fe85cfbae42a54cee2b292988a7d76728731e
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:53:31 +0100

    feat: add basic styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import styles from "./NavBar.module.scss";
 import SearchBar from "../SearchBar";
 
 export default class NavBar extends Component {
   render() { 
     return (
-      <>
+      <nav className={styles.navBar}>
         <p>Navbar</p>
         <SearchBar placeholder="Search movies..." />
-      </>
+      </nav>
     );
   }
 }
