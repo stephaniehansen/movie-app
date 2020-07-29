cb30493383c02e0a96bd4e42ed9b8dbc93063a9e
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 13:27:31 +0100

    feat: add basic styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from "./Card.module.scss";
 
 export default class Card extends Component {
   render() { 
     const {Title, Year, Poster} = this.props.movie;
     return (
-        <div>
+        <div className={styles.card}>
           <img src={Poster} alt={Title} />
           <h3>{Title}</h3>
           <p>{Year}</p>
