c2fd7327f16e5758b351e29d5b69c6443ccd04cf
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:24:20 +0100

    feat: add CardList component

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class CardList extends Component {
+
+  render() { 
+    return (
+      <section>
+        <p>CardList</p>
+      </section>
+
+    );
+  }
+}
