8e7bbc162d258ca1f2920eb2f757ce8efe5cf3c0
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 21:26:42 +0100

    feat: refactor onInput

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,11 +3,11 @@ import styles from "./SearchBar.module.scss"
 
 export default class SearchBar extends Component {
   render() { 
-    const { placeholder, name, updateSearch, handleChange } = this.props;
+    const { placeholder, name, updateSearch, handleChange, style } = this.props;
 
     return ( 
-      <div class={styles.searchBar}>
-        <input type="text" name={name} placeholder={placeholder} onInput={updateSearch} onKeyPress={handleChange} />
+      <div class={styles.searchBar} style={style}>
+        <input type="text" name={name} placeholder={placeholder} onInput={(e) => updateSearch(e.target)} onKeyPress={handleChange} style={style}/>
       </div>
      );
   }
