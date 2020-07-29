28a3b6c33928136dab092e5e877d04fa23dbd3ea
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:31:23 +0100

    feat: import SearchBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from "../SearchBar";
 
 export default class NavBar extends Component {
   render() { 
     return (
-      <p>Navbar</p>
+      <>
+        <p>Navbar</p>
+        <SearchBar />
+      </>
     );
   }
 }
