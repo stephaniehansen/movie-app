df9bd95103fd51f2c8976d3264e2714941c08a32
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:25:14 +0100

    feat: display movies from sample data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 
 export default class Card extends Component {
   render() { 
+    const {Title, Year, Poster} = this.props.movie;
     return (
-      <p>Card</p>
+        <div>
+          <img src={Poster} alt={Title} />
+          <h3>{Title}</h3>
+          <p>{Year}</p>
+        </div>
     );
   }
 }
