4b7cbae79bf08b1fd958a6eb11e7e0f1fb2293a8
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 10:22:25 +0100

    chore: add sample data

---
+++ src/data/movies.js
@@ -0,0 +1,124 @@
+const movies = [
+  {
+    "Title": "Harry Potter and the Sorcerer's Stone",
+    "Year": "2001",
+    "Rated": "PG",
+    "Released": "16 Nov 2001",
+    "Runtime": "152 min",
+    "Genre": "Adventure, Family, Fantasy",
+    "Director": "Chris Columbus",
+    "Writer": "J.K. Rowling (novel), Steve Kloves (screenplay)",
+    "Actors": "Richard Harris, Maggie Smith, Robbie Coltrane, Saunders Triplets",
+    "Plot": "An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.",
+    "Language": "English",
+    "Country": "UK, USA",
+    "Awards": "Nominated for 3 Oscars. Another 17 wins & 65 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNjQ3NWNlNmQtMTE5ZS00MDdmLTlkZjUtZTBlM2UxMGFiMTU3XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "7.6/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "81%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "64/100"
+        }
+    ],
+    "Metascore": "64",
+    "imdbRating": "7.6",
+    "imdbVotes": "621,298",
+    "imdbID": "tt0241527",
+    "Type": "movie",
+    "DVD": "28 May 2002",
+    "BoxOffice": "$317,557,891",
+    "Production": "Warner Bros. Pictures",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  {
+    "Title": "Mean Girls",
+    "Year": "2004",
+    "Rated": "PG-13",
+    "Released": "30 Apr 2004",
+    "Runtime": "97 min",
+    "Genre": "Comedy",
+    "Director": "Mark Waters",
+    "Writer": "Rosalind Wiseman (book), Tina Fey (screenplay)",
+    "Actors": "Lindsay Lohan, Rachel McAdams, Tina Fey, Tim Meadows",
+    "Plot": "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.",
+    "Language": "English, German, Vietnamese, Swahili",
+    "Country": "USA, Canada",
+    "Awards": "7 wins & 25 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "7.0/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "84%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "66/100"
+        }
+    ],
+    "Metascore": "66",
+    "imdbRating": "7.0",
+    "imdbVotes": "321,942",
+    "imdbID": "tt0377092",
+    "Type": "movie",
+    "DVD": "21 Sep 2004",
+    "BoxOffice": "$85,974,306",
+    "Production": "Paramount Pictures",
+    "Website": "N/A",
+    "Response": "True"
+  },
+  {
+    "Title": "The Lizzie McGuire Movie",
+    "Year": "2003",
+    "Rated": "PG",
+    "Released": "02 May 2003",
+    "Runtime": "94 min",
+    "Genre": "Adventure, Comedy, Family, Music, Romance",
+    "Director": "Jim Fall",
+    "Writer": "Susan Estelle Jansen, Ed Decter, John J. Strauss, Terri Minsky (characters)",
+    "Actors": "Hilary Duff, Adam Lamberg, Hallie Todd, Robert Carradine",
+    "Plot": "Lizzie McGuire has graduated from middle school and takes a trip to Rome, Italy with her class. And what was supposed to be only a normal trip, becomes a teenager's dream come true.",
+    "Language": "English, Italian",
+    "Country": "USA",
+    "Awards": "2 wins & 3 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTMzNDg1NDc0NF5BMl5BanBnXkFtZTYwOTY2OTU3._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "5.5/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "41%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "56/100"
+        }
+    ],
+    "Metascore": "56",
+    "imdbRating": "5.5",
+    "imdbVotes": "34,855",
+    "imdbID": "tt0306841",
+    "Type": "movie",
+    "DVD": "12 Aug 2003",
+    "BoxOffice": "$42,672,630",
+    "Production": "Walt Disney Pictures",
+    "Website": "N/A",
+    "Response": "True"
+  },
+]
+
+export default movies;
