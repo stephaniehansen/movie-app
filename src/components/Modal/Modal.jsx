5921895adce0f6906a68e94e0b6bb9b41e8f80c4
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-29 22:17:25 +0100

    feat: add structure

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,25 @@ import styles from "./Modal.module.scss"
 
 export default class Modal extends Component {
   render() { 
-    const { Title } = this.props.currentMovie;
+    const { Title, Plot, Runtime, Genre, Poster } = this.props.currentMovie;
 
+    console.log(this.props.currentMovie);
     return (
-      <div className={styles.modal}>
+      <div className={styles.modal} onClick={this.props.closeModal}>
         <div className={styles.modalContainer}>
-          <span onClick={this.props.closeModal}>close</span>
-          <h1>{Title}</h1>
+          <div className={styles.close} onClick={this.props.closeModal}>close</div>
+          <div className={styles.modalContent}>
+            <div className={styles.moviePoster}>
+              <img src={Poster} alt={Title} />
+            </div>
+            <div className={styles.movieInfo}>
+              <h2>{Title}</h2>
+              <div>
+                <span>{Runtime}</span> | <span>{Genre}</span>
+              </div>
+              <p>{Plot}</p>
+            </div>
+          </div>
         </div>
       </div>
     );
