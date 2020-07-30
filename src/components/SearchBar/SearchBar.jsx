e4314c530a104972207d94726a1c6e894a83a421
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-30 10:17:46 +0100

    feat: update input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,11 +3,11 @@ import styles from "./SearchBar.module.scss"
 
 export default class SearchBar extends Component {
   render() { 
-    const { placeholder, name, updateSearch, handleChange, style } = this.props;
+    const { type, placeholder, name, updateSearch, handleChange, style } = this.props;
 
     return ( 
       <div class={styles.searchBar} style={style}>
-        <input type="search" name={name} placeholder={placeholder} onInput={(e) => updateSearch(e.target)} onKeyPress={handleChange} style={style}/>
+        <input type={type} name={name} placeholder={placeholder} onInput={(e) => updateSearch(e.target)} onKeyPress={handleChange} style={style}/>
       </div>
      );
   }
