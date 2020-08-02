b921d4c0d356fd34bc1e38d2636db043025ef58e
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 12:11:31 +0100

    feat: add logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,14 +3,18 @@ import styles from "./NavBar.module.scss";
 import SearchBar from "../SearchBar";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import logo from "../../assets/logo.png"
 
 export default class NavBar extends Component {
   render() {
     const { updateSearch, handleChange } = this.props;
     return (
       <nav className={styles.navBar}>
-        <div class={styles.logo}>
-          <h2 onClick={() => window.location.reload()}>Movies</h2>
+        <div class={styles.logo} onClick={() => window.location.reload()}>
+          <span>
+            <img src={logo} />
+          </span>
+          <div className={styles.logoText}> <span>Find</span>Movies</div>
         </div>
         <div className={styles.searchContainer}>
           <SearchBar type="search" name="title" placeholder="Search movies by title" updateSearch={updateSearch} handleChange={handleChange} style={{width: "100%", marginRight: "10px"}} />
