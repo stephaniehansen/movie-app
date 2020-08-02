beed38f971e3c55749c9a0c4b9be929bc230e0af
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 15:58:51 +0100

    Updates

---
+++ precache-manifest.5436a5a17818c3adbe0d4b4737812dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfeabcb8cd622de3b6398d34a2da718",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "a882bb85d09bb0bee351",
+    "url": "/movie-app/static/css/main.695b07f9.chunk.css"
+  },
+  {
+    "revision": "43c11cf42e9a56437fe5",
+    "url": "/movie-app/static/js/2.63f1d8f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.63f1d8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a882bb85d09bb0bee351",
+    "url": "/movie-app/static/js/main.73af4a3e.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
--- src/data/fa-library.js
+++
@@ -1,4 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
-
-export default library.add(faSearch, faTimes);
