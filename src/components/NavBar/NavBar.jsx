10274e1f88e67fecc3728f7398a571af8909a8b5
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 13:25:51 +0100

    feat: show/hide input fields on icon click

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,8 +6,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from "../../assets/logo.png"
 
 export default class NavBar extends Component {
+  state = {
+    displaySearch: false
+  }
+
+  toggleSearch = () => this.setState({ displaySearch: !this.state.displaySearch });
+
   render() {
     const { updateSearch, handleChange } = this.props;
+
+    const searchInputs = this.state.displaySearch ? (
+      <>
+        <div className={styles.searchTitle}>
+          <SearchBar type="search" name="title" placeholder="Search movies by title" updateSearch={updateSearch} handleChange={handleChange} style={{width: "100%", marginRight: "10px"}} />
+        </div>
+        <div className={styles.searchYear}>
+          <SearchBar type="text" name="year" placeholder="Release year" updateSearch={updateSearch} handleChange={handleChange} />
+        </div> 
+      </>
+    ) : null;
+
     return (
       <nav className={styles.navBar}>
         <div class={styles.logo} onClick={() => window.location.reload()}>
@@ -17,9 +35,8 @@ export default class NavBar extends Component {
           <div className={styles.logoText}> <span>Find</span>Movies</div>
         </div>
         <div className={styles.searchContainer}>
-          <SearchBar type="search" name="title" placeholder="Search movies by title" updateSearch={updateSearch} handleChange={handleChange} style={{width: "100%", marginRight: "10px"}} />
-          <SearchBar type="text" name="year" placeholder="Release year" updateSearch={updateSearch} handleChange={handleChange} />
-          <span className={styles.searchIcon} onClick={handleChange}>
+          {searchInputs}
+          <span className={styles.searchIcon} onClick={this.toggleSearch}>
             <FontAwesomeIcon icon="search" />
           </span>
         </div>
