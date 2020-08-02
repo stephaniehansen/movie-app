cffedf1ba411a07fa41326d5eb4a8784bfd2eb6c
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 13:54:51 +0100

    Updates

---
+++ precache-manifest.8bf6b8be4133492a1034787902ad0912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1195293c11af491e8716af36d1e607",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "68e33852584b7992fd98",
+    "url": "/movie-app/static/css/main.695b07f9.chunk.css"
+  },
+  {
+    "revision": "43c11cf42e9a56437fe5",
+    "url": "/movie-app/static/js/2.63f1d8f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.63f1d8f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e33852584b7992fd98",
+    "url": "/movie-app/static/js/main.52eb8931.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  }
+]);
