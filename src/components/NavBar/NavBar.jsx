e3771bf0fed218039003356ab4af7df2eef70adb
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 17:50:44 +0100

    feat: change to search using icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,26 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from "../../assets/logo.png"
 
 export default class NavBar extends Component {
-  state = {
-    displaySearch: false
-  }
-
-  toggleSearch = () => this.setState({ displaySearch: !this.state.displaySearch });
-
   render() {
     const { updateSearch, handleChange } = this.props;
-
-    const searchInputs = this.state.displaySearch ? (
-      <>
-        <div className={styles.searchTitle}>
-          <SearchBar type="search" name="title" placeholder="Search movies by title" updateSearch={updateSearch} handleChange={handleChange} style={{width: "100%", marginRight: "10px"}} />
-        </div>
-        <div className={styles.searchYear}>
-          <SearchBar type="search" name="year" placeholder="Release year" updateSearch={updateSearch} handleChange={handleChange} />
-        </div> 
-      </>
-    ) : null;
-
     return (
       <nav className={styles.navBar}>
         <div className={styles.logo} onClick={() => window.location.reload()}>
@@ -35,8 +17,13 @@ export default class NavBar extends Component {
           <div className={styles.logoText}> <span>Find</span>Movies</div>
         </div>
         <div className={styles.searchContainer}>
-          {searchInputs}
-          <span className={styles.searchIcon} onClick={this.toggleSearch}>
+          <div className={styles.searchTitle}>
+            <SearchBar type="search" name="title" placeholder="Search movies by title" updateSearch={updateSearch} handleChange={handleChange} style={{width: "100%", marginRight: "10px"}} />
+          </div>
+          <div className={styles.searchYear}>
+            <SearchBar type="search" name="year" placeholder="Release year" updateSearch={updateSearch} handleChange={handleChange} />
+          </div> 
+          <span className={styles.searchIcon} onClick={handleChange}>
             <FontAwesomeIcon icon="search" />
           </span>
         </div>
